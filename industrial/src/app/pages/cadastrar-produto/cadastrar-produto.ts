679047335cf3e8affe2eb592b97868399cab7cc7
pedrohenrimartins <129549967+pedrohenrimartins@users.noreply.github.com>
2026-04-02 21:00:09 -0300

    Comecei a pagina de cadastro de produtos

---
+++ industrial/src/app/pages/cadastrar-produto/cadastrar-produto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-produto',
+  imports: [],
+  templateUrl: './cadastrar-produto.html',
+  styleUrl: './cadastrar-produto.css',
+})
+export class CadastrarProduto {}
